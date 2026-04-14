75c297e5827afd4b5b253c6eb04b1075d62b6a65
visualplayerxd <visualplayerxd@gmail.com>
2026-04-14 22:20:04 +0700

    Update main.cpp

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -1,80 +1,75 @@
 #include <cstring>
-#include <thread>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <jni.h>
+#include <pthread.h>
 #include <unistd.h>
-#include <cinttypes>
-#include "hack.h"
+#include <dlfcn.h>
+#include "hook.h"
 #include "zygisk.hpp"
-#include "game.h"
-#include "log.h"
+#include "il2cpp_dump.h"
 
 using zygisk::Api;
 using zygisk::AppSpecializeArgs;
 using zygisk::ServerSpecializeArgs;
 
+static void *il2cpp_handle = nullptr;
+static char dump_dir[PATH_MAX];
+
+// Оригинальная функция dlopen
+static void *(*orig_dlopen)(const char *filename, int flag);
+
+// Наш хук
+static void *my_dlopen(const char *filename, int flag) {
+    void *handle = orig_dlopen(filename, flag);
+    
+    if (filename && strstr(filename, "libil2cpp.so")) {
+        LOGI("libil2cpp.so loaded at %p", handle);
+        il2cpp_handle = handle;
+        
+        // Запускаем дамп в отдельном потоке через 2 секунды
+        pthread_t tid;
+        pthread_create(&tid, nullptr, [](void*) -> void* {
+            sleep(2);
+            if (il2cpp_handle) {
+                il2cpp_dump(il2cpp_handle, dump_dir);
+                dump_libunity(dump_dir);
+            }
+            return nullptr;
+        }, nullptr);
+        pthread_detach(tid);
+    }
+    
+    return handle;
+}
+
 class MyModule : public zygisk::ModuleBase {
 public:
     void onLoad(Api *api, JNIEnv *env) override {
-        this->api = api;
-        this->env = env;
+        env_ = env;
     }
 
     void preAppSpecialize(AppSpecializeArgs *args) override {
-        auto package_name = env->GetStringUTFChars(args->nice_name, nullptr);
-        auto app_data_dir = env->GetStringUTFChars(args->app_data_dir, nullptr);
-        preSpecialize(package_name, app_data_dir);
-        env->ReleaseStringUTFChars(args->nice_name, package_name);
-        env->ReleaseStringUTFChars(args->app_data_dir, app_data_dir);
+        if (!args || !args->nice_name) {
+            LOGE("Skip unknown process");
+            return;
+        }
+        
+        if (isGame(env_, args->app_data_dir)) {
+            enable_hack = true;
+            strcpy(dump_dir, args->app_data_dir);
+        }
     }
 
     void postAppSpecialize(const AppSpecializeArgs *) override {
         if (enable_hack) {
-            std::thread hack_thread(hack_prepare, game_data_dir, data, length);
-            hack_thread.detach();
+            // Устанавливаем хук на dlopen
+            DobbyHook((void*)dlopen, (void*)my_dlopen, (void**)&orig_dlopen);
+            LOGI("dlopen hook installed");
         }
     }
 
 private:
-    Api *api;
-    JNIEnv *env;
-    bool enable_hack;
-    char *game_data_dir;
-    void *data;
-    size_t length;
-
-    void preSpecialize(const char *package_name, const char *app_data_dir) {
-        if (strcmp(package_name, GamePackageName) == 0) {
-            LOGI("detect game: %s", package_name);
-            enable_hack = true;
-            game_data_dir = new char[strlen(app_data_dir) + 1];
-            strcpy(game_data_dir, app_data_dir);
-
-#if defined(__i386__)
-            auto path = "zygisk/armeabi-v7a.so";
-#endif
-#if defined(__x86_64__)
-            auto path = "zygisk/arm64-v8a.so";
-#endif
-#if defined(__i386__) || defined(__x86_64__)
-            int dirfd = api->getModuleDir();
-            int fd = openat(dirfd, path, O_RDONLY);
-            if (fd != -1) {
-                struct stat sb{};
-                fstat(fd, &sb);
-                length = sb.st_size;
-                data = mmap(nullptr, length, PROT_READ, MAP_PRIVATE, fd, 0);
-                close(fd);
-            } else {
-                LOGW("Unable to open arm file");
-            }
-#endif
-        } else {
-            api->setOption(zygisk::Option::DLCLOSE_MODULE_LIBRARY);
-        }
-    }
+    JNIEnv *env_{};
+    bool enable_hack = false;
 };
 
 REGISTER_ZYGISK_MODULE(MyModule)
